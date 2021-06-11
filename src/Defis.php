c0aad72fbf8e6b64b4e66326af5b2dc8d551c813
Cedric-blg <belanger.cedric@gmail.com>
2021-06-11 09:27:50 +0200

    :tada: initial commit

---
+++ src/Defis.php
@@ -0,0 +1,52 @@
+<?php
+class Defis
+{
+    //Convertir les heures en secondes
+    public static function howManySeconds($hours)
+    {
+
+    }
+
+    //Compte le nombre de patte dans la ferme
+    public static function animals($chickens, $cows, $pigs)
+    {
+
+    }
+
+    //Convertir l'âge en jour (365 ans)
+    public static function calcAge($age)
+    {
+
+    }
+
+    //Retourner la première valeur du tableau
+    public static function getFirstValue($array)
+    {
+
+    }
+
+    //Remplacer les voyelles de la chaîne de caractère par le signe (str_replace)
+    public static function replaceVowels($str, $ch)
+    {
+
+    }
+
+    //Retourne un tableau de valeur avec la première lettre seulement en majuscule (ucfirst, strtolower)
+    public static function capMe($arr)
+    {
+
+    }
+
+    //Convertir le nombre en binaire et compter le nombre de 1 (decbin, substr_count)
+    public static function countOnes($i)
+    {
+
+    }
+
+    //Retourne un tableau sans les doublons (in_array)
+    public static function removeDups($arr)
+    {
+
+    }
+
+}
