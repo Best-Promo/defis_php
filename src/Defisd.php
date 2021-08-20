85d33f3119e292d2b0a7e47ace0cd294d213d466
Cedric-blg <belanger.cedric@gmail.com>
2021-08-20 11:35:22 +0200

    new battle D

--- src/Defisd.php
+++ src/Defisd.php
@@ -4,34 +4,30 @@ class Defisd
     //Compte le nombre d'entrée dans un tableau
     public static function countValue($arr)
     {
-        return count($arr); 
+
     }
     
     //Récupérer le contenu du fichier
     public static function readFile($file)
     {
-        return file_get_contents($file);
+
     }
     
     //Supprimer les espaces avant et après
     public static function deleteSpace($txt)
     {
-        return trim($txt);
+
     }
     
     //Supprimer les espaces avant et après
     public static function checkMoreTen($arr)
     {
-        return array_filter($arr, function($val){
-            if($val > 10){
-                return $val;
-            }
-        });
+
     }
     
-    //Supprimer les espaces avant et après
+    //Compare les valeurs de deux tableaux (ou plus) et renvoie les différences.
     public static function sendDiff($arr1, $arr2)
     {
-        return array_diff($arr1, $arr2);
+
     }
 }
